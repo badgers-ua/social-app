5f3c5c9cae4c7cd8aa24a1d8e84ea216400ee427
yurii.badger <badger.lviv.ua@gmail.com>
2022-06-18 20:00:14 +0300

    [lemon] - updated api types

--- apps/lemon/src/app/hooks/api/useUnFollowApi.ts
+++ apps/lemon/src/app/hooks/api/useUnFollowApi.ts
@@ -3,13 +3,13 @@ import { useAxios } from '../../providers/AxiosProvider';
 import { AxiosInstance } from 'axios';
 import useApi from './useApi';
 
-const useUnFollowApi = (): LazyApi<string[]> => {
-  const { request, isLoading, data, error } = useApi<string[]>();
+const useUnFollowApi = (): LazyApi<void> => {
+  const { request, isLoading, data, error } = useApi<void>();
   const axios: AxiosInstance = useAxios();
 
   const load = async (id: string) => {
     const requestHandler = async () =>
-      await axios.patch<string[]>('user/unfollow/', null, { params: { id } });
+      await axios.patch<void>('user/unfollow/', null, { params: { id } });
 
     await request(requestHandler);
   };
